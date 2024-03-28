1e0199fb256d0dd28533edd91153cb9eb444382d
M buda <buda.mindaugas@gmail.com>
2024-03-28 19:50:00 +0200

    changed the design of nav bar as it should be

---
+++ index.js
@@ -0,0 +1,7 @@
+/* function burgerNavigation(event) {
+    event.preventDefault();
+    let hamburger = document.getElementById("nav-menu");
+    hamburger.classList.toggle("active");
+}
+
+document.querySelector('.hamburger').addEventListener('click', burgerNavigation); */
